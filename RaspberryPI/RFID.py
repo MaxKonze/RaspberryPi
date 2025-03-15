0f31c677c660f78f0c33b5b31d461512ce9c1a47
Maximilian <konmax1801@gho365.de>
2025-03-15 18:17:10 +0100

    fix #11

--- RaspberryPI/RFID.py
+++ RaspberryPI/RFID.py
@@ -1,8 +1,8 @@
-from RFID import MFRC522
+import MFRC522
 import time
 
 # Create an object of the class MFRC522
-mfrc = MFRC522.MFRC522()
+mfrc = MFRC522()
 
 def read_uid():
     print("Place the RFID card near the reader...")
