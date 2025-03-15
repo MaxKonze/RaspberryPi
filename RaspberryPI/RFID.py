605edf6bd2a58e5886d39738c4be5e781921285d
Maximilian <konmax1801@gho365.de>
2025-03-15 18:13:30 +0100

    fix #10

--- RaspberryPI/RFID.py
+++ RaspberryPI/RFID.py
@@ -7,23 +7,22 @@ mfrc = MFRC522.MFRC522()
 def read_uid():
     print("Place the RFID card near the reader...")
     
-    while True:
-        # Scan for cards
-        (status, TagType) = mfrc.MFRC522_Request(mfrc.PICC_REQIDL)
+    (status, TagType) = mfrc.MFRC522_Request(mfrc.PICC_REQIDL)
         
-        # If a card is found
+    if status == mfrc.MI_OK:
+        print("Card detected")
+            
+        (status, uid) = mfrc.MFRC522_Anticoll()
+            
         if status == mfrc.MI_OK:
-            print("Card detected")
+            print(f"Card UID: {' '.join(map(str, uid))}")  
             
-            # Get the UID of the card
-            (status, uid) = mfrc.MFRC522_Anticoll()
-            
-            # If we have the UID, print it
-            if status == mfrc.MI_OK:
-                print(f"Card UID: {' '.join(map(str, uid))}")
-                break  # Exit after reading the card UID
+            return True
 
         time.sleep(1)
 
 if __name__ == "__main__":
-    read_uid()
+    while True:
+        st = read_uid()
+        if st:
+            break
